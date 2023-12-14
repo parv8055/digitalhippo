b9e91c929c76bc5d264c6a3887c57ecc654a7038
parv8055 <choudharyparv4.com>
2023-12-14 14:54:16 +0530

    cart sheet

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,14 +1,83 @@
 "use Client";
 
-import { Sheet, SheetTrigger } from "./ui/sheet";
+import { ShoppingCart } from "lucide-react";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { Separator } from "./ui/separator";
+import { formatPrice } from "@/lib/utils";
+import { Button, buttonVariants } from "./ui/button";
+import Link from "next/link";
+import Image from "next/image";
 
 const Cart = () => {
-  return <Sheet>
-  <SheetTrigger>Open</SheetTrigger>
-  
-</Sheet>
+  const itemCount = 0;
+  const fee = 1;
+  return (
+    <Sheet>
+      <SheetTrigger className="group -m-2 flex items-center p-2">
+        <ShoppingCart
+          aria-hidden="true"
+          className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
+        />
+        <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
+          0
+        </span>
+      </SheetTrigger>
+      <SheetContent className="flex w-full flex-col pr-0 sm:max-w-lg">
+        <SheetHeader className="space-y-2.5 pr-6">
+          <SheetTitle>Cart (0)</SheetTitle>
+        </SheetHeader>
+        {itemCount > 0 ? (
+          <>
+            <div className="flex w-full flex-col pr-6">cart items</div>
+            <div className="space-y-4 pr-6">
+              <Separator />
+              <div className="space-y-1.5 pr-6">
+                <div className="flex">
+                  <span className="flex-1">Shipping</span>
+                  <span>Free</span>
+                </div>
+                <div className="flex">
+                  <span className="flex-1">Transaction Fee</span>
+                  <span>{formatPrice(fee)}</span>
+                </div>
+                <div className="flex">
+                  <span className="flex-1">Total</span>
+                  <span>{formatPrice(fee)}</span>
+                </div>
+              </div>
+              <SheetFooter>
+                <SheetClose asChild>
+                  <Link
+                    href="/cart"
+                    className={buttonVariants({ className: "w-full" })}
+                  >
+                    Continue to checkout
+                  </Link>
+                </SheetClose>
+              </SheetFooter>
+            </div>
+          </>
+        ) : (
+          <div className="flex flex-col items-center justify-center h-full space-y-1">
+            <div
+              aria-hidden="true"
+              className="relative mb-4 h-60 w-60 text-muted-foreground"
+            >
+              <Image src="/hippo-emplty-cart.png" alt="empty cart image" fill />
+            </div>
+          </div>
+        )}
+      </SheetContent>
+    </Sheet>
+  );
+};
 
-}
-
-
-export default Cart
+export default Cart;
