c62d84cd286ed41e1650ef8578052d2bb615771d
parv8055 <choudharyparv4.com>
2023-12-14 13:18:11 +0530

    cart sheet

---
+++ src/components/Cart.tsx
@@ -0,0 +1,14 @@
+"use Client";
+
+import { Sheet, SheetTrigger } from "./ui/sheet";
+
+const Cart = () => {
+  return <Sheet>
+  <SheetTrigger>Open</SheetTrigger>
+  
+</Sheet>
+
+}
+
+
+export default Cart
