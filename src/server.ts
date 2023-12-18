ce0972de6cb3bb915109393db513b7612486cb1e
parv8055 <choudharyparv4.com>
2023-12-18 13:25:56 +0530

    sign up

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,39 @@
 import express from "express";
 import { nextApp, nextHandler } from "./next-utils";
 import { getPayloadClient } from "./get-payload";
+import * as trpcExpress from '@trpc/server/adapters/express'
+import { appRouter } from "./trpc";
 
 const app = express();
 const PORT: number = Number(process.env.PORT) || 3000;
-
+const createContext = ({
+  req,
+  res,
+}: trpcExpress.CreateExpressContextOptions) => ({
+  req,
+  res,
+});
 const start = async () => {
   const payload = await getPayloadClient({
     initOptions: {
       express: app,
       onInit: async (cms) => {
-        cms.logger.info(`Admin URL: ${cms.getAdminURL()}`)
+        cms.logger.info(`Admin URL: ${cms.getAdminURL()}`);
       },
     },
-  })
-
-  app.use((req,res)=>nextHandler(req,res))
+  });
+  app.use(
+    "/api/trpc",
+    trpcExpress.createExpressMiddleware({
+      router: appRouter,
+      createContext,
+    })
+  );
+  app.use((req, res) => nextHandler(req, res));
   nextApp.prepare().then(() => {
     app.listen(PORT, async () => {
-     console.log(`Listening on http://localhost:${PORT}`);
-    })
-  })
+      console.log(`Listening on http://localhost:${PORT}`);
+    });
+  });
 };
-start()
+start();
