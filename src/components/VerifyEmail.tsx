e707969f05035414acacc3c508c55f6a4fedb804
parv8055 <choudharyparv4.com>
2023-12-19 19:02:12 +0530

    done

--- src/components/VerifyEmail.tsx
+++ src/components/VerifyEmail.tsx
@@ -1,58 +1,75 @@
-"use client";
-import { trpc } from "@/trpc/client";
-import { Loader, XCircle } from "lucide-react";
-import Image from "next/image";
-import React from "react";
-import { buttonVariants } from "./ui/button";
-import Link from "next/link";
+'use client'
+
+import { trpc } from '@/trpc/client'
+import { Loader2, XCircle } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { buttonVariants } from './ui/button'
 
 interface VerifyEmailProps {
-  token: string;
+  token: string
 }
+
 const VerifyEmail = ({ token }: VerifyEmailProps) => {
-  const { data, isLoading, isError } = trpc.auth.verifyEmail.useQuery({
-    token,
-  });
+  const { data, isLoading, isError } =
+    trpc.auth.verifyEmail.useQuery({
+      token,
+    })
+
   if (isError) {
     return (
-      <div className="flex flex-col items-center gap-2">
-        <XCircle className="w-8 h-8 text-red-600" />
-        <h3 className="font-semibold text-xl">There was a problem</h3>
-        <p className="text-muted-foreground text-sm text-center">
-          This token is not valid or might be expired. <br />
-          Please try again
+      <div className='flex flex-col items-center gap-2'>
+        <XCircle className='h-8 w-8 text-red-600' />
+        <h3 className='font-semibold text-xl'>
+          There was a problem
+        </h3>
+        <p className='text-muted-foreground text-sm'>
+          This token is not valid or might be expired.
+          Please try again.
         </p>
       </div>
-    );
+    )
   }
+
   if (data?.success) {
     return (
-      <div className="flex h-full flex-col items-center justify-center">
-        <div className="relative mb-4 h-60 w-60 text-muted-foreground">
-          <Image src="/hippo-email-sent.png" fill alt="hippo email sent" />
+      <div className='flex h-full flex-col items-center justify-center'>
+        <div className='relative mb-4 h-60 w-60 text-muted-foreground'>
+          <Image
+            src='/hippo-email-sent.png'
+            fill
+            alt='the email was sent'
+          />
         </div>
 
-        <h3 className="font-semibold text-2xl">You&apos;re all set!</h3>
-        <p className="text-muted-foreground text-center mt-1">
-          Thank you for verifying your email
+        <h3 className='font-semibold text-2xl'>
+          You&apos;re all set!
+        </h3>
+        <p className='text-muted-foreground text-center mt-1'>
+          Thank you for verifying your email.
         </p>
-        <Link className={buttonVariants({ className: "mt-4" })} href="/sign-in">
+        <Link
+          className={buttonVariants({ className: 'mt-4' })}
+          href='/sign-in'>
           Sign in
         </Link>
       </div>
-    );
+    )
   }
+
   if (isLoading) {
     return (
-      <div className="flex flex-col items-center gap-2">
-        <Loader className="animate-spin w-8 h-8 text-zinc-300" />
-        <h3 className="font-semibold text-xl">Verifying...</h3>
-        <p className="text-muted-foreground text-sm text-center">
+      <div className='flex flex-col items-center gap-2'>
+        <Loader2 className='animate-spin h-8 w-8 text-zinc-300' />
+        <h3 className='font-semibold text-xl'>
+          Verifying...
+        </h3>
+        <p className='text-muted-foreground text-sm'>
           This won&apos;t take long.
         </p>
       </div>
-    );
+    )
   }
-};
+}
 
-export default VerifyEmail;
+export default VerifyEmail
